bc5c3700a00c5f9be253f8444f8dfa4d4369d646
will-barnett99 <willbarnett99@hotmail.com>
2025-09-19 15:22:17 +0100

    fixing broken util tests

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -2,19 +2,21 @@ const {
   convertTimestampToDate
 } = require("../db/seeds/utils");
 
+require("jest-extended")
+
 describe("convertTimestampToDate", () => {
   test("returns a new object", () => {
     const timestamp = 1557572706232;
     const input = { created_at: timestamp };
     const result = convertTimestampToDate(input);
     expect(result).not.toBe(input);
-    expect(result).toBeObject();
+    expect(typeof result).toBe("object");
   });
   test("converts a created_at property to a date", () => {
     const timestamp = 1557572706232;
     const input = { created_at: timestamp };
     const result = convertTimestampToDate(input);
-    expect(result.created_at).toBeDate();
+    expect(result.created_at).toBeInstanceOf(Date);
     expect(result.created_at).toEqual(new Date(timestamp));
   });
   test("does not mutate the input", () => {
--- models/articles-model.js
+++ models/articles-model.js
@@ -14,12 +14,14 @@ const fetchArticles = (sort_by = 'created_at', order = 'desc') => {
         'comment_count'
     ];
 
+    const validOrders = ['asc', 'desc']
+
     if(!validColumns.includes(sort_by)) {
         return Promise.reject({status:400, msg: 'Invalid sort_by column'});
     }
 
     const lowerOrder = order.toLowerCase();
-    if(!['asc', 'desc'].includes(lowerOrder)){
+    if(!validOrders.includes(lowerOrder)){
         return Promise.reject({status:400, msg:'Invalid order value'})
     }
 
@@ -33,7 +35,8 @@ const fetchArticles = (sort_by = 'created_at', order = 'desc') => {
          COUNT(comments.article_id)::INT AS "comment_count" 
          FROM articles 
          LEFT JOIN comments ON comments.article_id = articles.article_id 
-         GROUP BY articles.article_id;`).then(({rows: articles}) => {
+         GROUP BY articles.article_id
+         ORDER BY ${sort_by} ${order};`).then(({rows: articles}) => {
         return articles;
     });
     
